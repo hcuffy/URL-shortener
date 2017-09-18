d3cf28503093fd8d82b931eeaac1fb94c703c8f8
System <none>
2017-09-18 06:36:06 +0000

    🍙🏎 Updated with Glitch

--- shorten.js
+++ shorten.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var express = require('express');
 var app = express();
 var db;
-var id;
+
 
 
 data.connectToServer(function (err) {
@@ -12,66 +12,73 @@ data.connectToServer(function (err) {
 
 });
 
+function display(shortform, req, res) {
+    var link = req.params[0];
 
-module.exports.create = function (req, res) {
-    var yoururl = req.params[0];
+    res.json({
+        Origin: link,
+        Shortened: 'https://power-debt.glitch.me/' + shortform
+    });
 
+}
 
+function addToDB(yoururl, req, res) {
+    var place = {
+        'original_url': yoururl,
+    };
 
-    function display() {
+    db.collection('addresslist').insert(place, function (err, doc) {
+        var id = doc.insertedIds[0].toString();
+        var newid = id.substring(id.length - 5);
 
-        console.log('in check');
-        db.collection('addresslist').find({
-            original_url: yoururl
-        }, {
-            _id: 0
-        }).toArray(function (err, ans) {
-
-            if (err) throw err;
-
-            res.redirect({ans
-                
-            });
-
-        });
-
-    }
-
-    function addToDB() {
-        var place = {
-            'original_url': yoururl,
+        var newplace = {
+            $set: {
+                'shortform': newid
+            }
         };
 
-        db.collection('addresslist').insert(place, function (err, doc) {
-            var id = doc.insertedIds[0].toString();
-            var newid = id.substring(id.length - 5);
+        if (err) throw err;
+        console.log('Inserted Document');
 
-            var newplace = {
-                $set: {
-                    'shortform': 'https://power-debt.glitch.me/' + newid
-                }
-            };
+        db.collection("addresslist").update(place, newplace, function (err, ans) {
 
             if (err) throw err;
-            console.log('Inserted Document');
-
-            db.collection("addresslist").update(place, newplace, function (err, ans) {
-
-                if (err) throw err;
-                console.log("Document updated");
-                display();
-
-            });
-
-
-            //    res.json(doc.insertedIds[0]);
-
+            console.log("Document updated");
+            display(newid, req, res);
         });
 
-    }
+    });
+}
 
 
+module.exports.get = function (req1, res1) {
 
+    var id = req1.params[0];
+
+    db.collection('addresslist').find({
+        shortform: req1.params.shortform
+    }, {
+        _id: 0
+    }).toArray(function (err, ans) {
+
+        if (err) throw err;
+        if (ans.length > 0) {
+            res1.redirect(ans[0].original_url
+
+            );
+            db.close();
+        } else {
+            res1.send('The link was not found')
+        }
+    });
+
+}
+
+module.exports.new = function (req, res) {
+
+
+    var yoururl = req.params[0];
+    console.log(yoururl);
     if (!isUrl(yoururl)) {
         res.json({
             "Error": 'Invalid URL',
@@ -79,21 +86,18 @@ module.exports.create = function (req, res) {
 
     } else if (isUrl(yoururl)) {
 
-        db.collection('addresslist').count({
+        db.collection('addresslist').find({
             'original_url': yoururl
-        }).then(function (nums) {
-
-            if (nums > 0) {
-                display();
+        }).toArray(function (err, ans) {
+            console.log(ans);
+            if (ans.length > 0) {
+                display(ans[0].shortform, req, res);
             } else {
-                addToDB();
+                addToDB(yoururl, req, res);
             }
 
 
         });
-
-
-
 
     }
 }
