50306c5476494e4bdd197069a572542e867aecfc
System <none>
2017-10-07 13:12:04 +0000

    🚄🍔 Updated with Glitch

--- datab.js
+++ datab.js
@@ -1,5 +1,6 @@
 var mongodb = require('mongodb');
-var url = 'mongodb://short:HaveFun@ds123614.mlab.com:23614/shortener';
+var url = process.env.URL;
+var assert = require('assert');
 var MongoClient = mongodb.MongoClient;
 var database;
 
